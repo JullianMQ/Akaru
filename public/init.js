81dfd7970bc8c4b1590401af8fcfecd37a35f046
Jullian Quiambao <jullianmq@gmail.com>
2024-09-15 21:25:06 +0800

    started to work on firestore

--- public/init.js
+++ public/init.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-    apiKey: "AIzaSyCSOjiTqbAd4g2rLn8cIyMHbdKSjgNvIhk",
-    authDomain: "firstdemo-c1177.firebaseapp.com",
-    projectId: "firstdemo-c1177",
-    storageBucket: "firstdemo-c1177.appspot.com",
-    messagingSenderId: "344621487288",
-    appId: "1:344621487288:web:d040d43c33d3e26be5104d",
-    measurementId: "G-B38YCVL1SP"
+  apiKey: "AIzaSyB0CO9PloCBIieVifCPi5ptBibNTRj4fZk",
+  authDomain: "test-lms-addbase.firebaseapp.com",
+  projectId: "test-lms-addbase",
+  storageBucket: "test-lms-addbase.appspot.com",
+  messagingSenderId: "770512950395",
+  appId: "1:770512950395:web:9ad0a7fe3caadb35c53ede",
+  measurementId: "G-WC1HFBNBDH"
 };
 
 // Firebase stuff
