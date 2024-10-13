465346a6aa46bd70b33e8740075eb1842f3d7cfe
Ces <francesluis23@gmail.com>
2024-10-14 03:15:00 +0800

    frontend design for "borrowed books" page,  removed "change password" button, and added hamburger button

--- src/app/index_frontend.js
+++ src/app/index_frontend.js
@@ -1,15 +1,10 @@
 var hamburger = document.getElementById('hamburger');
 var trigger = document.getElementById('header');
-var icon = document.getElementById('icon');
+
 
 hamburger.onclick = function () {
     hamburger.classList.toggle('open');
     trigger.classList.toggle('open');
     document.body.classList.toggle('menu-open');
-    if (icon.getAttribute("src") === "public/images/user-profile-dark.png") {
-        icon.setAttribute("src", "public/images/user-profile.png");
-    } else {
-        icon.setAttribute("src", "public/images/user-profile-dark.png");
-    }
 }
 
--- src/app/user_borrowed_books.js
+++ src/app/user_borrowed_books.js
@@ -149,3 +149,13 @@ const removeBookFromUserBorrowedBooks = async (userId, bookId) => {
 }
 
 window.onload = getBooks();
+
+var hamburger = document.getElementById('hamburger');
+var trigger = document.getElementById('header');
+
+
+hamburger.onclick = function () {
+    hamburger.classList.toggle('open');
+    trigger.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+}
