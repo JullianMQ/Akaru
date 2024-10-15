2a8630563e84c2fc06fd24ce9251b059fe0242a0
Ces <francesluis23@gmail.com>
2024-10-15 18:33:38 +0800

    added item hover effects

--- src/app/index_frontend.js
+++ src/app/index_frontend.js
@@ -1,10 +1,8 @@
 var hamburger = document.getElementById('hamburger');
 var trigger = document.getElementById('header');
 
-
 hamburger.onclick = function () {
     hamburger.classList.toggle('open');
     trigger.classList.toggle('open');
     document.body.classList.toggle('menu-open');
 }
-
